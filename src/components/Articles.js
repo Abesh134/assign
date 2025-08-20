e2f3e4a3da95a4f1b27d59b116003f31185046c1
Abesh134 <abeshbarua2004@gmail.com>
2025-08-20 22:50:16 +0600

    picture adding in sidebar

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -2,14 +2,27 @@ import React, {useState} from "react";
 import { Table, Badge, Pagination } from "react-bootstrap";
 
 const articlesData =[
-     { title: "Do tour best", status: "Published" , edited: "2 days ago" },
+     { title: "Do tour best", status: "Published" , edited: "1 days ago" },
     { title: "Legends fall", status: "Published" , edited: "2 days ago" },
-    { title: "no return", status: "Published" , edited: "2 days ago" },
+    { title: "Digital Classrooms: Boon or Bane?", status: "Warning" , edited: "2 days ago" },
+    { title: "How Reading Habits Shape Young Minds", status: "Warning" , edited: "2 days ago" },
+    { title: "The Role of Teachers in a Tech-Driven World", status: "Published" , edited: "3 days ago" },
+    { title: "Why Failure Teaches Best", status: "Published" , edited: "3 days ago" },
+    { title: "no return", status: "Warning" , edited: "3 days ago" },
+    { title: "Exams vs. Skills: What Should We Prioritize?", status: "Warning" , edited: "4 days ago" },
+    { title: "The Language of Knowledge", status: "Published" , edited: "4 days ago" },
+    { title: "The Power of Curiosity", status: "Warning" , edited: "4 days ago" },
+    { title: "Global Citizens Through Education", status: "Warning" , edited: "5 days ago" },
+    { title: "The Future of Online Learning", status: "Published" , edited: "5 days ago" },
+    { title: "Unleashing Student Potential", status: "Warning" , edited: "6 days ago" },
+    { title: "Why Emotional Intelligence Should Be Taught in Schools", status: "Warning" , edited: "6 days ago" },
+    { title: "The Future of Learning: Beyond Classrooms", status: "Published" , edited: "6 days ago" },
+    
 ]
 
 export default function Articles(){
     const [page, setPage] = useState(1);
-    const articlesPerPage =2 ;
+    const articlesPerPage =4 ;
     const totalPages = Math.ceil(articlesData.length/articlesPerPage);
     const displayedArticles = articlesData.slice((page-1)* articlesPerPage,page*articlesPerPage);
       return(
